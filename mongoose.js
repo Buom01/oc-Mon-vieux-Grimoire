838f02de48b5342c2924a58868be95b4ba1365dd
Bastien ADAM <bastien.adam.buom01@gmail.com>
2024-07-11 19:41:27 +0200

    Add authentication APIs

--- mongoose.js
+++ mongoose.js
@@ -7,8 +7,8 @@ async function run() {
     await mongoose.connect(uri, clientOptions);
     await mongoose.connection.db.admin().command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
+  } catch(e) {
+    // Ensures that the client will close on error
     await mongoose.disconnect();
   }
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,62 @@
-var express = require('express');
-var router = express.Router();
-var User = require('../things/user');
+const express = require('express');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const router = express.Router();
+const User = require('../things/user');
 
-/* GET users listing.
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-*/
+router.post(
+  '/signup',
+  async function(req, res, next)
+  {
+    const {email, password} = req.body;
+
+    try
+    {
+      const hashedPassword = password && await bcrypt.hash(password, 10);
+      const user = new User({
+        email,
+        password: hashedPassword
+      });
+
+      await user.save();
+      res.status(201).json({message: 'Utilisateur crée !'});
+    }
+    catch(error)
+    {
+      console.error(error);
+      res.status(400).json({message: error.message})
+    };
+  }
+);
+
+router.post(
+  '/login',
+  async function(req, res, next)
+  {
+    const {email, password} = req.body;
+
+    try
+    {
+      const user = await User.findOne({email});
+
+      if (!user || !password || !(await bcrypt.compare(password, user.password)))
+        return res.status(401).json({message: "Paire login/mot de passe incorrecte"});
+
+      res.status(200).json({
+        userId: user._id,
+        token: jwt.sign(
+          {userId: user._id},
+          'RANDOM_DEV_SECRET_KEY',
+          {expiresIn: '24h'}
+        )
+      });
+    }
+    catch(error)
+    {
+      console.error(error);
+      res.status(400).json({error})
+    };
+  }
+);
 
 module.exports = router;
