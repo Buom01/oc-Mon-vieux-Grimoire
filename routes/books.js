2593efa5a9ea6abce5af3c143946d5682c6f454b
Bastien ADAM <bastien.adam.buom01@gmail.com>
2024-07-16 19:14:43 +0200

    Add public book APIs

--- routes/books.js
+++ routes/books.js
@@ -2,9 +2,54 @@ var express = require('express');
 var router = express.Router();
 var Book = require('../things/book');
 
-/* GET books. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get(
+  '/',
+  async function(req, res, next)
+  {
+    try
+    {
+      res.json(await Book.find());
+    }
+    catch(error)
+    {
+      console.error(error);
+      res.status(400).json({message: error.message})
+    };
+  }
+);
+
+router.get(
+  '/:id',
+  async function(req, res, next)
+  {
+    const id = req.params.id;
+
+    try
+    {
+      res.json(await Book.findById(id));
+    }
+    catch(error)
+    {
+      console.error(error);
+      res.status(400).json({message: error.message})
+    };
+  }
+);
+
+router.get(
+  '/bestrating',
+  async function(req, res, next)
+  {
+    try
+    {
+      res.json(await (Book.find().sort({averageRating: -1}).limit(3)));
+    }
+    catch(error)
+    {
+      console.error(error);
+      res.status(400).json({message: error.message})
+    };
+  }
+);
 
 module.exports = router;
