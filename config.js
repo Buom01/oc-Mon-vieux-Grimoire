e718631e616e3df8c567b061bcb686343b8d7200
Bastien ADAM <bastien.adam.buom01@gmail.com>
2024-07-17 15:39:14 +0200

    Configuration is overridden by envs for prod + extract port to config

--- config.js
+++ config.js
@@ -1,13 +1,16 @@
-// Should be defined by env
+// Using envs value for production, while defaults aim to be used in developpement
+const {JWT_SECRET_KEY, BACKEND_HOSTNAME, FRONTEND_HOSTNAME, MONGO_URI, PORT} = process.env;
 
-const jwtSecretKey = 'RANDOM_DEV_SECRET_KEY';
-const backendHostname = 'http://localhost:4000';
-const frontendHostname = 'http://localhost:3000';
-const mongoUri = 'mongodb+srv://bastienadambuom01:kaykWXj65EcaKn8a@ocmonvieuxgrimoire.sz9gl7b.mongodb.net/?retryWrites=true&w=majority&appName=ocMonVieuxGrimoire';
+const jwtSecretKey = JWT_SECRET_KEY ?? 'RANDOM_DEV_SECRET_KEY';
+const backendHostname = BACKEND_HOSTNAME ?? 'http://localhost:4000';
+const frontendHostname = FRONTEND_HOSTNAME ?? 'http://localhost:3000';
+const mongoUri = MONGO_URI ?? 'mongodb+srv://bastienadambuom01:kaykWXj65EcaKn8a@ocmonvieuxgrimoire.sz9gl7b.mongodb.net/?retryWrites=true&w=majority&appName=ocMonVieuxGrimoire';
+const port = PORT ?? '4000';
 
 module.exports = {
     jwtSecretKey,
     backendHostname,
     frontendHostname,
-    mongoUri
+    mongoUri,
+    port
 };
