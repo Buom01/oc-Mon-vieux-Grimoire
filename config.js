1541722ac1a67bbc342b0a724d27fc47e701a589
Bastien ADAM <bastien.adam.buom01@gmail.com>
2024-07-17 15:27:30 +0200

    Extract mongo URI to config file

--- config.js
+++ config.js
@@ -3,9 +3,11 @@
 const jwtToken = 'RANDOM_DEV_SECRET_KEY';
 const backendHostname = 'http://localhost:4000';
 const frontendHostname = 'http://localhost:3000';
+const mongoUri = 'mongodb+srv://bastienadambuom01:kaykWXj65EcaKn8a@ocmonvieuxgrimoire.sz9gl7b.mongodb.net/?retryWrites=true&w=majority&appName=ocMonVieuxGrimoire';
 
 module.exports = {
     jwtToken,
     backendHostname,
-    frontendHostname
+    frontendHostname,
+    mongoUri
 };
--- mongoose.js
+++ mongoose.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
-const uri = "mongodb+srv://bastienadambuom01:kaykWXj65EcaKn8a@ocmonvieuxgrimoire.sz9gl7b.mongodb.net/?retryWrites=true&w=majority&appName=ocMonVieuxGrimoire";
+const {mongoUri} = require('./config');
+
 const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
 async function run() {
   try {
     // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
-    await mongoose.connect(uri, clientOptions);
+    await mongoose.connect(mongoUri, clientOptions);
     await mongoose.connection.db.admin().command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } catch(e) {
