01071c4b40d57f320c6e5c58a1c9f95b4ad91805
Bastien Adam <Buom01@users.noreply.github.com>
2024-08-20 15:23:21 +0200

    Remove default MONGO_URI

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const {JWT_SECRET_KEY, BACKEND_HOSTNAME, FRONTEND_HOSTNAME, MONGO_URI, PORT} = p
 const jwtSecretKey = JWT_SECRET_KEY ?? 'RANDOM_DEV_SECRET_KEY';
 const backendHostname = BACKEND_HOSTNAME ?? 'http://localhost:4000';
 const frontendHostname = FRONTEND_HOSTNAME ?? 'http://localhost:3000';
-const mongoUri = MONGO_URI ?? 'mongodb+srv://bastienadambuom01:kaykWXj65EcaKn8a@ocmonvieuxgrimoire.sz9gl7b.mongodb.net/?retryWrites=true&w=majority&appName=ocMonVieuxGrimoire';
+const mongoUri = MONGO_URI;
 const port = PORT ?? '4000';
 
 module.exports = {
